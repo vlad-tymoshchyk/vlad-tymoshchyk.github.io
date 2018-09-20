487465932239ff415803ee6f7dc63eaa32f5cf7c
vlad-tymoshchyk <vlsdyslav.sky@gmail.com>
2018-09-20 12:36:07 +0300

    -default-

--- pwa-one/script.js
+++ pwa-one/script.js
@@ -1,1 +1,23 @@
-console.log("hi");
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+	.then(response => {
+		response.json()
+	})
+	.then(data => {
+		console.log(data);
+		console.log("text");
+	})
+
+console.log("data:");
+
+function curl(url) {
+	let req = new XMLHttpRequest();
+	req.open('get', url, true);
+	req.send();
+	req.onreadystatechange = function(response) {
+		if(req.readyState == '4') {
+			console.log("done: " + response);
+			window.resp = response;
+		}
+	}
+}
+
